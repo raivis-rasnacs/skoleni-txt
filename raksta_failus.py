1cf8678776f78f82b83c0dd9088b5e28c71c5764
Raivis <71266644+raivis-rasnacs@users.noreply.github.com>
2022-11-23 22:47:55 +0200

    Update raksta_failus.py

--- raksta_failus.py
+++ raksta_failus.py
@@ -6,7 +6,7 @@ def raksta_failus(skoleni_pa_klasem: dict) -> None:
     for klase in skoleni_pa_klasem:
 
         # veido failu ar doto nosaukumu, piemēram, 11c.txt
-        with open("jauns_fails.txt", "a", encoding="UTF-8") as jauns_fails:
+        with open("jauns_fails.txt", "w", encoding="UTF-8") as jauns_fails:
 
             pass 
             # TODO: ieraksta failā visus klases skolēnus katru jaunā rindā
